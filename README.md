4f10328ecdf6e4b1dda8a66f84c20fa72198036b
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-01-31 00:53:31 +0100

    Add NFS volume for data, use deploy bash script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # wolke
-Run Nextcloud via Docker
+
+Run Nextcloud via Docker with NFS data storage, Redis, MariaDB and LetsEncrypt certificate.
+
+## Usage
+
+```
+git clone git@github.com:adzialocha/wolke.git
+cd wolke
+
+cp example.env .env
+vim .env
+
+./deploy.sh
+```
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f .env ]
+then
+  echo "Error: .env not found"
+  exit
+fi
+
+# Delete all containers with a `wolke_` prefix
+docker rm -f `docker ps -aq -f name=wolke_*`
+
+# Export environment variables
+set -a
+source .env
+
+# Populate variables and deploy it!
+cat compose.yml | envsubst | docker-compose -f - -p wolke_ up -d
